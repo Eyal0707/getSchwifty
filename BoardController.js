828f15807513e58b709dfe8f748b4be8b7b50551
Eyal Florentin <eyal0707@gmail.com>
2022-07-03 17:59:01 +0300

    added way to move by using number only

--- BoardController.js
+++ BoardController.js
@@ -3,37 +3,52 @@ export default class BoardController {
     this.gameboard = gameboard;
   }
 
-  ZeroCheck(column, row) {
-    if (row < this.gameboard.length) {
-      if (column < this.gameboard[column].length) {
-        return this.gameboard[(row, column)];
+  IsValidSlot(column, row) {
+    if (row < this.gameboard.length && row > 0) {
+      if (column < this.gameboard[row].length && column > 0) {
+        return this.gameboard[row][column] == 0;
       }
     }
     return false;
   }
 
   Move(originColumn, originRow, destColumn, destRow) {
-    destValue = this.gameboard[destRow][destColumn];
+    let destValue = this.gameboard[destRow][destColumn];
     this.gameboard[destRow][destColumn] =
       this.gameboard[originRow][originColumn];
+    this.gameboard[originRow][originColumn] = destValue;
   }
 
-  TryMove(column, row) {
-    if (this.gameboard[(row, column)] > 0) {
-      if (this.ZeroCheck(column + 1, row)) {
+  TryMoveWithCoords(column, row) {
+    if (this.gameboard[row][column] > 0) {
+      if (this.IsValidSlot(column + 1, row)) {
         this.Move(column, row, column + 1, row);
         return true;
-      } else if (this.ZeroCheck(column - 1, row)) {
+      } else if (this.IsValidSlot(column - 1, row)) {
         this.Move(column, row, column - 1, row);
         return true;
-      } else if (this.ZeroCheck(column, row + 1)) {
+      } else if (this.IsValidSlot(column, row + 1)) {
         this.Move(column, row, column, row + 1);
         return true;
-      } else if (this.ZeroCheck(column, row - 1)) {
+      } else if (this.IsValidSlot(column, row - 1)) {
         this.Move(column, row, column, row - 1);
         return true;
       }
+      console.log("no empty space around");
     }
+    return false;
+  }
+
+  TryMove(number) {
+    console.log(number);
+    for (let row = 0; row < this.gameboard.length; row++) {
+      for (let column = 0; column < this.gameboard.length; column++) {
+        if (this.gameboard[row][column] == number) {
+          return this.TryMoveWithCoords(column, row);
+        }
+      }
+    }
+    console.log("couldn't find number in board");
     return false;
   }
 }
--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+import BoardController from "./BoardController.js";
+
 const testGameBoard = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 0],
 ];
+
+const testingBoardController = new BoardController(testGameBoard);
+
+console.log(testGameBoard);
+
+const buttons = [
+  document.getElementById("1"),
+  document.getElementById("2"),
+  document.getElementById("3"),
+  document.getElementById("4"),
+  document.getElementById("5"),
+  document.getElementById("6"),
+  document.getElementById("7"),
+  document.getElementById("8"),
+];
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => {
+    console.log(testingBoardController.TryMove(i + 1));
+    console.log(testGameBoard);
+  });
+}
